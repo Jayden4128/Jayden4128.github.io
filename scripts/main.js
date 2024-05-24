1f2f72a8fcf32e94e40a654164d0ec40f940ca96
JDen <jaydenmgarcia@gmail.com>
2024-05-24 04:51:20 -0700

    Added Games Section Spotify Button and Link to Portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,3 @@ function printMessage()
     document.getElementById('helllo').innerHTML = alert("Hello! Have a Great Day!");
    
 }
-var img = new Image();
-img.src = 'Steam_icon_logo.svg.webp';
-img.onclick = function()
-{
-    window.location.href = 'https://steamcommunity.com/profiles/76561198079348284/';
-};
-document.body.appendChild(img);
